f0f8e6047bfec479b3b528ca7940988941d7006b
anyelser1213 <anyelserperez@gmail.com>
2022-08-30 20:55:46 -0400

    agregando una variable en español

--- resources/lang/es/variable.php
+++ resources/lang/es/variable.php
@@ -13,6 +13,7 @@ return [
     'search_yacht_charter' => 'Buscar yates en alquiler',
     'selling_yacht' => '¿Vender un yate?',
     'selling_yacht_subtitle' => 'cuenta con uno de los equipos de consultores de ventas más capacitados de la industria. Gracias al acceso a una red global, conocimiento del mercado y recursos de marketing, encontraremos un comprador para su yate en un plazo razonable y al precio adecuado.',
+    'find_out_more' => 'Más información',
     'superyacht_owner' => '¿Propietario de superyate?',
     'offers_services' => 'ofrece servicios en todos los campos relacionados con la navegación, incluida la gestión de chárter, gestión de yates, servicios de agencia, colocación de tripulación y seguros. Estamos aquí para ayudarlo en cada paso de la propiedad de su yate.',
 ]
